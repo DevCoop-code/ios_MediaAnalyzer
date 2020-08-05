e3a3f29b148992856f4305ae6537aa3d443fdb7e
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-08-05 15:56:44 +0900

    Study about iOS Document, Temp Directory
    All of iOS Application has their own Document & Temp Directory

--- mediaAnalyzer/ViewController/LMListTableViewController.swift
+++ mediaAnalyzer/ViewController/LMListTableViewController.swift
@@ -21,6 +21,18 @@ class LMListTableViewController: UIViewController {
         lmTableView.delegate = self
         lmTableView.dataSource = self
 
+        // Get the local video files
+        var fileMangr = FileManager.default
+        NSLog("File Directory path \(fileMangr.currentDirectoryPath) ")
+        
+        // Get the document directory path
+        let dirPaths = fileMangr.urls(for: .documentDirectory, in: .userDomainMask)
+        NSLog("File Document Directory path \(dirPaths[0].path) ")
+        
+        // Get tthe temporary directory path
+        let tmpDir = NSTemporaryDirectory()
+        NSLog("File Temporary Directory path \(tmpDir) ")
+        
         mediaFileArray = ["bip bop", "big buck bunny", "alticast atlantic girls in the lonely island"]
         
         lmTableView.estimatedRowHeight = 50
