d035a3a87e18484b25e2f752a6ddbfab41ce2869
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-08-05 15:43:15 +0900

    feat: add some comment

--- mediaAnalyzer/ViewController/LMListTableViewController.swift
+++ mediaAnalyzer/ViewController/LMListTableViewController.swift
@@ -17,6 +17,7 @@ class LMListTableViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Set tableview delegate
         lmTableView.delegate = self
         lmTableView.dataSource = self
 
