a4afe38c1d327b670db227f7b58778a5db423729
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-08-07 09:13:25 +0900

    feat: Add Navigation Controller to Navigate Local file list

--- mediaAnalyzer/ViewController/DrawVideoViewController.swift
+++ mediaAnalyzer/ViewController/DrawVideoViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class DrawVideoViewController: UITableViewController {
+class DrawVideoViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
