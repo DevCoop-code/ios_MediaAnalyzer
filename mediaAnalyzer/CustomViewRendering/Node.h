f365b071b846eaf647267e989e2051a2a1e831e7
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-09-05 08:46:19 +0900

    feat: Fill out the Node Class

--- mediaAnalyzer/CustomViewRendering/Node.h
+++ mediaAnalyzer/CustomViewRendering/Node.h
@@ -7,10 +7,33 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "BufferProvider.h"
+#import "Vertex.h"
+
+@import QuartzCore;
+@import Metal;
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface Node : NSObject
+
+@property(nonatomic) id<MTLDevice> device;
+@property(nonatomic) NSString* name;
+@property(nonatomic) NSInteger vertexCount;
+@property(nonatomic) id<MTLBuffer> vertexBuffer;
+@property(nonatomic) CFTimeInterval time;
+@property(nonatomic) id<MTLTexture> ytexture;
+@property(nonatomic) id<MTLTexture> cbcrtexture;
+
+- (instancetype) init:(NSString*)name
+               vertex:(NSArray<Vertex*>*)vertices
+               device:(id<MTLDevice>)device;
+
+- (void) render:(id<MTLCommandQueue>)commandQueue
+renderPipelineState:(id<MTLRenderPipelineState>) pipelineState
+       drawable:(id<CAMetalDrawable>)drawable
+    pixelBuffer:(CVPixelBufferRef)pixelBuffer
+     clearColor:(MTLClearColor*)clearColor;
 
 @end
 
