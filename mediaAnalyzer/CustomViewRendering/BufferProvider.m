f38f70c0c49de33da6eaf1d8c6a0da3bcf134477
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-09-04 16:40:18 +0900

    feat: Make BufferProvider class

---
+++ mediaAnalyzer/CustomViewRendering/BufferProvider.m
@@ -0,0 +1,38 @@
+//
+//  BufferProvider.m
+//  mediaAnalyzer
+//
+//  Created by HanGyo Jeong on 2020/09/04.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+#import "BufferProvider.h"
+
+@implementation BufferProvider
+{
+    // Store the Buffers themselves
+    NSMutableArray<MTLBuffer>* _uniformsBuffers;
+    
+    // Index of the next available buffer
+    NSInteger _availableBufferIndex;
+}
+
+// Create number of Buffers
+- (instancetype) init:(id<MTLDevice>)device
+ inflightBuffersCount:(NSInteger)inflightBuffersCount
+ sizeOfUniformsBuffer:(NSInteger)sizeOfUniformsBuffer
+{
+    _availableResourcesSemaphore = dispatch_semaphore_create(inflightBuffersCount);
+    
+    _inflightBufferCount = inflightBuffersCount;
+    _uniformsBuffers = (id)[NSMutableArray new];
+    
+    for (int i = 0; i < inflightBuffersCount; i++)
+    {
+        id<MTLBuffer> uniformsBuffer = [device newBufferWithLength:sizeOfUniformsBuffer
+                                                           options:MTLResourceCPUCacheModeWriteCombined];
+        [_uniformsBuffers addObject:uniformsBuffer];
+    }
+    return self;
+}
+@end
