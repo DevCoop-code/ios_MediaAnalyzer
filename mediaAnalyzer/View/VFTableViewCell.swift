97b61900feab526f5c7be6e67e7f9e0f6c78242e
HanGyo Jeong <tkdydwkdlfma90@gmail.com>
2020-09-24 09:55:09 +0900

    feat: Make TableViewCell which contained video frame

---
+++ mediaAnalyzer/View/VFTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  VFTableViewCell.swift
+//  mediaAnalyzer
+//
+//  Created by HanGyo Jeong on 2020/09/24.
+//  Copyright © 2020 HanGyoJeong. All rights reserved.
+//
+
+import UIKit
+
+class VFTableViewCell: UITableViewCell {
+    @IBOutlet weak var mediaFrameType: UILabel!
+}
